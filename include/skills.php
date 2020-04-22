f4b8384863f2834226ef72d07f25d0e9f9726a54
lucas lacroix <lacroixlucas59@gmail.com>
2020-04-22 11:48:12 +0200

    Fixation des marges maximale (responsive grand écran)

--- include/skills.php
+++ include/skills.php
@@ -1,34 +1,36 @@
 <div class="skills content">
-	<h3>ce dont je suis capable</h3>
-	<div class="skill gauche">
-		<img class="reveal bounceIn delay-1s" data-revealation="animated" src="img/lens.svg">
-		<p>Très tôt passionné de photographie, mes expériences me permettent
-			aujourd'hui d'apporter du contenu photographique aux projets.</p>
-	</div>
+	<div class="margium-s">
+		<h3>ce dont je suis capable</h3>
+		<div class="skill gauche">
+			<img class="reveal bounceIn delay-1s" data-revealation="animated" src="img/lens.svg">
+			<p>Très tôt passionné de photographie, mes expériences me permettent
+				aujourd'hui d'apporter du contenu photographique aux projets.</p>
+		</div>
 
-	<div class="skill droite">
-		<img class="reveal bounceIn delay-1s" data-revealation="animated" src="img/play-button.svg">
-		<p>Vidéo institutionnelle, vidéo produit, interview, motion design, dessin
-			animé ou encore fiction, je suis capable de réaliser tout type de vidéos.</p>
-	</div>
+		<div class="skill droite">
+			<img class="reveal bounceIn delay-1s" data-revealation="animated" src="img/play-button.svg">
+			<p>Vidéo institutionnelle, vidéo produit, interview, motion design, dessin
+				animé ou encore fiction, je suis capable de réaliser tout type de vidéos.</p>
+		</div>
 
-	<div class="skill gauche">
-		<img class="reveal bounceIn delay-1s" data-revealation="animated" src="img/design.svg">
-		<p>Photoshop, Illustrator, InDesign sont des outils de design et créations
-			graphiques que je maîtrise. Je peux donc répondre à toute demande
-			graphique.</p>
-	</div>
+		<div class="skill gauche">
+			<img class="reveal bounceIn delay-1s" data-revealation="animated" src="img/design.svg">
+			<p>Photoshop, Illustrator, InDesign sont des outils de design et créations
+				graphiques que je maîtrise. Je peux donc répondre à toute demande
+				graphique.</p>
+		</div>
 
-	<div class="skill droite">
-		<img class="reveal bounceIn delay-1s" data-revealation="animated" src="img/bullhorn.svg">
-		<p>Les réseaux sociaux, la rédaction, la promotion d'événement, de produit
-			ou de service sont des éléments que j'ai eu l'occasion d'expérimenter.
-			Je suis donc capable de réaliser toutes ces tâches.</p>
-	</div>
+		<div class="skill droite">
+			<img class="reveal bounceIn delay-1s" data-revealation="animated" src="img/bullhorn.svg">
+			<p>Les réseaux sociaux, la rédaction, la promotion d'événement, de produit
+				ou de service sont des éléments que j'ai eu l'occasion d'expérimenter.
+				Je suis donc capable de réaliser toutes ces tâches.</p>
+		</div>
 
-	<div class="skill gauche">
-		<img class="reveal bounceIn delay-1s" data-revealation="animated" src="img/data.svg">
-		<p>HTML, CSS, JavaScript, PHP sont des langages web que j'ai eu l'occasion
-	d'utiliser sur de nombreux projets. Je me spécialise en webdesign.</p>
+		<div class="skill gauche">
+			<img class="reveal bounceIn delay-1s" data-revealation="animated" src="img/data.svg">
+			<p>HTML, CSS, JavaScript, PHP sont des langages web que j'ai eu l'occasion
+		d'utiliser sur de nombreux projets. Je me spécialise en webdesign.</p>
+		</div>
 	</div>
 </div>
